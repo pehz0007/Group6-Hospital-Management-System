5c3d8138073159ab051c7adaa6b04be2061e1697
lermean <98082485+lermean@users.noreply.github.com>
2024-10-14 03:02:57 +0800

    added the doctor class

---
+++ src/main/java/com/group6/hms/app/models/Schedule.java
@@ -0,0 +1,37 @@
+package com.group6.hms.app.models;
+import com.group6.hms.app.models.Appointment;
+import java.util.ArrayList;
+
+public class Schedule {
+    private ArrayList<Appointment> appointments;
+
+    public Schedule() {
+        this.appointments = new ArrayList<>();
+    }
+
+    public void addAppointment(Appointment appointment) {
+        Appointment newAppointment = new Appointment();
+        newAppointment = appointment;
+        appointments.add(newAppointment);
+    }
+
+    public void removeAppointment(Appointment appointment) {
+
+    }
+
+    public void viewAppointments(ArrayList<Appointment> schedule) {
+
+    }
+
+    public Appointment updateAppointment(Appointment appointment) {
+        return appointment;
+    }
+
+    public ArrayList<Appointment> getAppointments() {
+        return this.appointments;
+    }
+
+    public ArrayList<Appointment> getUpcomingSchedule() {
+        return null;
+    }
+}
--- src/main/java/com/group6/hms/app/roles/DoctorRole.java
+++ src/main/java/com/group6/hms/app/roles/DoctorRole.java
@@ -1,8 +1,77 @@
 package com.group6.hms.app.roles;
+import java.sql.Array;
+import java.util.ArrayList;
 
 import com.group6.hms.framework.auth.Role;
+import com.group6.hms.app.roles.PatientRole;
+import com.group6.hms.app.models.Schedule;
+import com.group6.hms.app.models.Appointment;
+
 
 public class DoctorRole extends Role {
+    private int id;
+    private String name;
+    private ArrayList<PatientRole> patientRoles;
+    private Schedule schedules; // binary tree and hash table
+
+
+    public DoctorRole() {
+        this.id = 0;
+        this.name = null;
+        patientRoles = new ArrayList<>();
+        schedules = new Schedule();
+
+
+    }
+    public PatientRole getPatient() {
+        // search through the patientlist,
+        return null;
+    }
+    public void getAllPatients() {
+    }
+
+    public void addPatient(PatientRole patient) {
+
+    }
+
+    public void viewPatient(PatientRole patient) {
+        // use getPatient method to get the class
+        // Patient.getMedicalRecords
+    }
+
+    public boolean updatePatient(PatientRole patient) {
+        return false; // never update properly
+    }
+
+    public void viewSchedule(Schedule schedule) {
+
+    }
+
+    public boolean viewPersonalSchedule() {
+        ArrayList<Appointment> schedules= new ArrayList<>();
+        schedules = this.schedules.getAppointments();
+        this.schedules.viewAppointments(schedules);
+        return false;
+    }
+
+    public void setAvailability(){
+
+    }
+
+    public boolean updateStatus() {
+        return false; // update unsuccessful
+    }
+
+    public void viewUpcomingAppointments() {
+        // get upcoming appointments from the schedule class
+    }
+
+    public void recordAppointmentOutcome(Appointment appointment) {
+        //update status
+    }
+
+
+
     @Override
     public String toString() {
         return "Doctor";
