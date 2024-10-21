fea0e6ad5fd76244983a0d2609c486509487fc9b
pehz0007 <pehz0007@e.ntu.edu.sg>
2024-10-21 22:48:00 +0800

    Add serialization storage provider and multi storage provider

---
+++ src/main/java/com/group6/hms/app/auth/SerializationMapStorageProvider.java
@@ -0,0 +1,29 @@
+package com.group6.hms.app.auth;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SerializationMapStorageProvider<K, T> implements MultiStorageProvider<K, T> {
+
+    private Map<K, StorageProvider<T>> map = new HashMap<>();
+
+    @Override
+    public StorageProvider<T> getStorageProvider(K key) {
+        if(map.get(key) == null) {
+            loadStorageProvider(key);
+        }
+        return map.get(key);
+    }
+
+
+    private void loadStorageProvider(K key) {
+        File file = new File(key.toString() + ".ser");
+        SerializationStorageProvider<T> storageProvider = new SerializationStorageProvider<>();
+        map.put(key, storageProvider);
+        if(file.exists()){
+            storageProvider.loadFromFile(file);
+        }
+    }
+
+}
