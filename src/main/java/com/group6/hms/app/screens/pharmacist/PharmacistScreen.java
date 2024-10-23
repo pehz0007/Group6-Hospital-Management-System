da4dea075f6ce32df297c5c3e5b2753b4a71ddc6
pehz0007 <pehz0007@e.ntu.edu.sg>
2024-10-23 23:02:58 +0800

    Display records in pages

--- src/main/java/com/group6/hms/app/screens/pharmacist/PharmacistScreen.java
+++ src/main/java/com/group6/hms/app/screens/pharmacist/PharmacistScreen.java
@@ -4,8 +4,11 @@ import com.group6.hms.app.auth.LogoutScreen;
 import com.group6.hms.app.managers.AppointmentManager;
 import com.group6.hms.app.models.AppointmentOutcomeRecord;
 import com.group6.hms.app.MedicationStatus;
+import com.group6.hms.app.models.Medication;
 import com.group6.hms.app.roles.Pharmacist;
 import com.group6.hms.app.screens.MainScreen;
+import com.group6.hms.framework.screens.pagination.PaginationTableScreen;
+import com.group6.hms.framework.screens.pagination.SinglePaginationTableScreen;
 
 import java.util.List;
 import java.util.Scanner;
@@ -13,7 +16,6 @@ import java.util.UUID;
 
 public class PharmacistScreen extends LogoutScreen {
     private final AppointmentManager appointmentManager = new AppointmentManager();
-    private final Scanner scanner = new Scanner(System.in);
 
     /**
      * Constructor to initialize the PharmacistScreen.
@@ -35,7 +37,6 @@ public class PharmacistScreen extends LogoutScreen {
 
     @Override
     protected void onLogout() {
-        scanner.close(); // close the scanner only if sure no further input
         newScreen(new MainScreen());
     }
 
@@ -50,19 +51,39 @@ public class PharmacistScreen extends LogoutScreen {
 
     private void viewAppointmentOutcomeRecords() {
         println("Enter the medication status to filter by (e.g., PENDING, DISPENSED): ");
-        String statusInput = scanner.nextLine().toUpperCase();
+        String statusInput = readString().toUpperCase();
         try {
             MedicationStatus status = MedicationStatus.valueOf(statusInput);
             List<AppointmentOutcomeRecord> records = appointmentManager.getAppointmentOutcomeRecordsByStatus(status);
 
-            if (records.isEmpty()) {
-                println("No records found with the specified medication status.");
-            } else {
-                println("Appointment Outcome Records:");
-                for (AppointmentOutcomeRecord record : records) {
-                    println("Record ID: " + record.getRecordId());
+
+            navigateToScreen(new SinglePaginationTableScreen<>("Records", records) {
+                @Override
+                public void displaySingleItem(AppointmentOutcomeRecord item) {
+                    println("===================================================");
+                    println("Appointment Outcome Records:");
+
+                    println("Record ID :" + item.getRecordId());
+                    println("Date of Appointment:" + item.getDateOfAppointment());
+                    println("Consultation Note:" + item.getConsultationNotes());
+                    println("Medication Status:" + item.getMedicationStatus());
+                    println("Patient ID:" + item.getPatientId());
+
+                    for (Medication medication : item.getPrescribedMedication()){
+                        println("Medication :" + medication.getName());
+                    }
+                    println("===================================================");
                 }
-            }
+            });
+
+//            if (records.isEmpty()) {
+//                println("No records found with the specified medication status.");
+//            } else {
+//                println("Appointment Outcome Records:");
+//                for (AppointmentOutcomeRecord record : records) {
+//                    println("Record ID: " + record.getRecordId());
+//                }
+//            }
         } catch (IllegalArgumentException e) {
             println("Invalid medication status. Please enter a valid status.");
         }
@@ -70,10 +91,10 @@ public class PharmacistScreen extends LogoutScreen {
 
     private void updatePrescriptionStatus() {
         println("Enter the ID of the appointment outcome record you want to update: ");
-        String recordId = scanner.nextLine();
+        String recordId = readString();
 
         println("Enter the new medication status (e.g., PENDING, DISPENSED): ");
-        String statusInput = scanner.nextLine().toUpperCase();
+        String statusInput = readString().toUpperCase();
         try {
             MedicationStatus status = MedicationStatus.valueOf(statusInput);
             // Get all records and find the one matching the given recordId
