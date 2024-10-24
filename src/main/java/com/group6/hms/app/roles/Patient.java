c2d58bb0cb7b59333b60fb0de0627a0ef59fd80b
Eileen <elim083@e.ntu.edu.sg>
2024-10-24 12:25:50 +0800

    Updated Patient.java

--- src/main/java/com/group6/hms/app/roles/Patient.java
+++ src/main/java/com/group6/hms/app/roles/Patient.java
@@ -1,11 +1,43 @@
 package com.group6.hms.app.roles;
 
 import com.group6.hms.app.auth.User;
+import java.time.LocalDate;
 
 public class Patient extends User {
-    public Patient(String username, char[] password) {
+    private String patientName;
+    private String dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String bloodType;
+    private String pastDiagnoses;
+
+
+    public Patient(String username, char[] password, String patientName,
+                   String dateOfBirth, String gender, String phoneNumber,
+                   String email, String bloodType) {
+
         super(username, password);
+        this.patientName = patientName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.bloodType = bloodType;
+        this.pastDiagnoses = pastDiagnoses;
     }
+
+    public String getPatientName() {return patientName;}
+    public String getDateOfBirth() {return dateOfBirth;}
+    public String getGender() {return gender;}
+    public String getPhoneNumber() {return phoneNumber;}
+    public String getEmail() {return email;}
+    public String getBloodType() {return bloodType;}
+    public String getPastDiagnoses() {return pastDiagnoses;}
+
+    //updatable info
+    public void setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber;}
+    public void setEmail(String email) {this.email = email;}
 
     @Override
     public String getRoleName() {
