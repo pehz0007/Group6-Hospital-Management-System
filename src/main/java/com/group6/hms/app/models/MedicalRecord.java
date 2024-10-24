32dec8a45a6085b0a6189b175e62d79d37405ddd
Eileen <elim083@e.ntu.edu.sg>
2024-10-24 13:24:52 +0800

    Implemented MedicalRecord

--- src/main/java/com/group6/hms/app/models/MedicalRecord.java
+++ src/main/java/com/group6/hms/app/models/MedicalRecord.java
@@ -1,4 +1,27 @@
 package com.group6.hms.app.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MedicalRecord {
+    private List<String> pastDiagnoses;
+
+    public MedicalRecord() {
+        this.pastDiagnoses = new ArrayList<>();
+    }
+
+    public void addDiagnose(String diagnoses) {
+        pastDiagnoses.add(diagnoses);
+    }
+
+    public void display(){
+        if(pastDiagnoses.isEmpty()){
+            System.out.println("No diagnoses recorded");
+        }else{
+            System.out.println("Your past diagnoses recorded: ");
+            for (String diagnoses : pastDiagnoses) {
+                System.out.println(diagnoses);
+            }
+        }
+    }
 }
