080fa255bc68d4ade8496a872993c2a27046b519
Eileen <elim083@e.ntu.edu.sg>
2024-10-27 18:06:14 +0800

    Updated PatientDataView, added email and phoneNumber

--- src/main/java/com/group6/hms/app/screens/patient/PatientDataView.java
+++ src/main/java/com/group6/hms/app/screens/patient/PatientDataView.java
@@ -10,17 +10,27 @@ public class PatientDataView {
 
     private String patientId;
     private String name;
-    private String email;
     private LocalDate dateOfBirth;
     private Gender gender;
+    private String email;
+    private String phoneNumber;
     private BloodType bloodType;
 
     public PatientDataView(Patient patient) {
         this.patientId = patient.getUserId();
         this.name = patient.getName();
-        this.email = patient.getContextInformation();
-        this.bloodType = patient.getMedicalRecord().getBloodType();
         this.dateOfBirth = patient.getMedicalRecord().getDateOfBirth();
+        this.gender = patient.getGender();
+        this.email = patient.getEmail();
+        this.phoneNumber = patient.getPhoneNumber();
+        this.bloodType = patient.getMedicalRecord().getBloodType();
     }
 
+    public String getPatientId() {return patientId;}
+    public String getName() {return name;}
+    public LocalDate getDateOfBirth() {return dateOfBirth;}
+    public Gender getGender() {return gender;}
+    public String getEmail() {return email;}
+    public String getPhoneNumber() {return phoneNumber;}
+    public BloodType getBloodType() {return bloodType;}
 }
