22b559043a2e72c863dada6007c9474334224ed4
Eileen <elim083@e.ntu.edu.sg>
2024-10-31 07:48:40 +0800

    Added getPastTreatments and Diagnoses method

--- src/main/java/com/group6/hms/app/roles/Patient.java
+++ src/main/java/com/group6/hms/app/roles/Patient.java
@@ -1,16 +1,30 @@
 package com.group6.hms.app.roles;
 
 import com.group6.hms.app.auth.User;
+import com.group6.hms.app.models.AppointmentOutcomeRecord;
 import com.group6.hms.app.models.MedicalRecord;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Patient extends User {
-
-    private String contextInformation; // Email
+    private String contactInformation; // Email
     private MedicalRecord medicalRecord;
+    private List<AppointmentOutcomeRecord> appointmentOutcomeRecord;
 
-    public Patient(String userId, char[] password, String name, Gender gender) {
+    public Patient(String userId, char[] password, String name, Gender gender, String contactInformation) {
         super(userId, password, name, gender);
+        this.contactInformation = contactInformation;
         this.medicalRecord = new MedicalRecord();
+        this.appointmentOutcomeRecord = new ArrayList<>();
+    }
+
+    public List<AppointmentOutcomeRecord> getPastTreatments(){
+        return appointmentOutcomeRecord;
+    }
+
+    public List<AppointmentOutcomeRecord> getPastDiagnoses(){
+        return appointmentOutcomeRecord;
     }
 
     public MedicalRecord getMedicalRecord() {
@@ -21,12 +35,12 @@ public class Patient extends User {
         this.medicalRecord = medicalRecord;
     }
 
-    public String getContextInformation() {
-        return contextInformation;
+    public String getContactInformation() {
+        return contactInformation;
     }
 
-    public void setContextInformation(String contextInformation) {
-        this.contextInformation = contextInformation;
+    public void setContactInformation(String contactInformation) {
+        this.contactInformation = contactInformation;
     }
 
     @Override
