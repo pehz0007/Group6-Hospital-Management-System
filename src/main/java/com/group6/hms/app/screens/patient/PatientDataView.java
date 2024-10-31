2435687ef69a0304d89cd96e4baa76ba1394aff8
Eileen <elim083@e.ntu.edu.sg>
2024-10-31 08:47:53 +0800

    Nothing changed

--- src/main/java/com/group6/hms/app/screens/patient/PatientDataView.java
+++ src/main/java/com/group6/hms/app/screens/patient/PatientDataView.java
@@ -19,6 +19,7 @@ public class PatientDataView {
         this.patientId = patient.getUserId();
         this.name = patient.getName();
         this.email = patient.getContactInformation();
+        this.gender = patient.getGender();
         this.bloodType = patient.getMedicalRecord().getBloodType();
         this.dateOfBirth = patient.getMedicalRecord().getDateOfBirth();
     }
