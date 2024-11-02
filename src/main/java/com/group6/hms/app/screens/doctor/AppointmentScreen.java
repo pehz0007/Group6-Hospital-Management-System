1ff6490e7f3f33489acff107fadb976f97df58dd
lermean <98082485+lermean@users.noreply.github.com>
2024-11-02 21:51:42 +0800

    appointment page

--- src/main/java/com/group6/hms/app/screens/doctor/AppointmentScreen.java
+++ src/main/java/com/group6/hms/app/screens/doctor/AppointmentScreen.java
@@ -45,7 +45,7 @@ public class AppointmentScreen extends CalendarScreen<Appointment, List<Appointm
     protected void handleOption(int optionId) {
         super.handleOption(optionId);
         userStorageProvider.loadFromFile(userFile);
-        doc = (Doctor) loginManager.getCurrentlyLoggedInUser();
+        doc = DoctorScreen.getDoctor();
         if(optionId == 5){
              List<Appointment> requests = appointmentManager.getAppointmentsByDoctorAndStatus(doc, AppointmentStatus.REQUESTED);
             navigateToScreen(new AcceptOrDeclineScreen(requests));
@@ -125,26 +125,7 @@ public class AppointmentScreen extends CalendarScreen<Appointment, List<Appointm
 
 
 
-    protected void acceptorDecline(ArrayList<Appointment> appointments) {
-        Scanner sc = new Scanner(System.in);
-        if(appointments.isEmpty()){
-            println("No requests appointment are found.\n");
-        }else{
-            println("You got "+ appointments.size()+" requests!");
-            for(Appointment appointment : appointments){
-                println("Scheduled Date: "+ appointment.getDate().toString());
-                println("Start Time: "+ appointment.getStartTime().toString());
-                println("End Time: "+ appointment.getEndTime().toString()+"\n");
-                println("Accept or decline?");
-                String result = sc.nextLine();
-                if(result.equalsIgnoreCase("accept")){
-                    appointmentManager.acceptAppointmentRequest(appointment);
-                }else{
-                    appointmentManager.declineAppointmentRequest(appointment);
-                }
-            }
-        }
-    }
+
 
     @Override
     public void onStart() {
@@ -156,94 +137,6 @@ public class AppointmentScreen extends CalendarScreen<Appointment, List<Appointm
                 .collect(groupingBy(Appointment::getDate));
     }
 
-    protected void upcomingAppointments(ArrayList<Appointment> appointments) {
-        Map<LocalDate, List<Appointment>> availabilityMap = new TreeMap<>();
-
-        for (Appointment upcoming : appointments) {
-            LocalDate date = upcoming.getDate();
-
-            availabilityMap.putIfAbsent(date, new ArrayList<>());
-            availabilityMap.get(date).add(upcoming);
-
-        }
-        for (Map.Entry<LocalDate, List<Appointment>> entry : availabilityMap.entrySet()) {
-            LocalDate date = entry.getKey();
-            List<Appointment> appointments1 = entry.getValue();
-
-
-            for (Appointment upcoming1 : appointments1) {
-                println("Upcoming Appointment Date: " + upcoming1.getDate().toString());
-                println("Patient Name: "+ upcoming1.getPatient().getName());
-                println("Appointment Timing: "+ upcoming1.getStartTime().toString()+"\n");
-            }
-            println(""); // Print a blank line for better readability
-        }
-    }
-
-    protected void RecordAppointmentDetails(ArrayList<Appointment> appointments) {
-        Map<LocalDate, List<Appointment>> availabilityMap = new HashMap<>();
-
-        for (Appointment upcoming : appointments) {
-            LocalDate date = upcoming.getDate();
-            if(LocalDate.now().isEqual(date)){
-                availabilityMap.putIfAbsent(date, new ArrayList<>());
-                availabilityMap.get(date).add(upcoming);
-            }
-        }
-        String message = "Appointments for today:";
-        println("-".repeat(message.length()));
-        Scanner sc = new Scanner(System.in);
-
-        for (Map.Entry<LocalDate, List<Appointment>> entry : availabilityMap.entrySet()) {
-            LocalDate date = entry.getKey();
-            List<Appointment> appointments1 = entry.getValue();
-            AppointmentService service1;
-
-            for (Appointment upcoming1 : appointments1) {
-                println("Patient Name: "+ upcoming1.getPatient().toString());
-                println("Appointment Timing: "+ upcoming1.getStartTime().toString());
-                println("Is this the appointment you want to update? (Y or N)");
-                char result = sc.nextLine().charAt(0);
-                if(result == 'Y'){
-                    println("Appointment Details Update:");
-                    String details = sc.nextLine();
-                    println("Appointment Service: (Consult or Xray or Blood Test");
-                    String service = sc.nextLine();
-                    if(service.equalsIgnoreCase("consult")){
-                        service1 = AppointmentService.CONSULT;
-                    }else if(service.equalsIgnoreCase("xray")){
-                        service1 = AppointmentService.XRAY;
-                    }else {
-                        service1 = AppointmentService.BLOOD_TEST;
-                    }
-
-//                    ArrayList<Medication> medications = new ArrayList<>();
-//                    medications.add(new Medication(UUID.randomUUID(), "Panadol"));
-//                    medications.add(new Medication(UUID.randomUUID(), "Cough Syrup"));
-//                    medications.add(new Medication(UUID.randomUUID(), "Flu Medicine"));
-//                    AppointmentOutcomeRecord appointmentOutcomeRecord = new AppointmentOutcomeRecord(upcoming1.getDoctor().getSystemUserId(), upcoming1.getPatient().getSystemUserId(), upcoming1.getDate(),
-//                            service1, medications, details, MedicationStatus.PENDING);
-//                    appointmentManager.completeAppointment(upcoming1, appointmentOutcomeRecord);
-//                    println("Updated Successfully!!");
-                    // TODO:
-                    //  1) get all medications
-                    //  2) doctor select what medications to prescribe + quantity
-                    //  3) create PrescribedMedication object to pass into AppointmentOutcomeRecord
-                    ArrayList<PrescribedMedication> medications = new ArrayList<>();
-//                    medications.add()
-//                    medications.add(new Medication(UUID.randomUUID(), "Panadol"));
-//                    medications.add(new Medication(UUID.randomUUID(), "Cough Syrup"));
-//                    medications.add(new Medication(UUID.randomUUID(), "Flu Medicine"));
-//                    AppointmentOutcomeRecord appointmentOutcomeRecord = new AppointmentOutcomeRecord(upcoming1.getDoctor().getUserId(), upcoming1.getPatient().getUserId(), upcoming1.getDate(),
-
-//                            service1, medications, details, MedicationStatus.PENDING);
-//                    appointmentManager.completeAppointment(upcoming1, appointmentOutcomeRecord);
-                    break;
-                }
-            }
-            println(""); // Print a blank line for better readability
-        }
-    }
 
 
 }
