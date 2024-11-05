f70b076086ab001e0dc9500279fc91196b7c2f6c
Eileen <elim083@e.ntu.edu.sg>
2024-11-05 19:54:48 +0800

    Patient Appointment System

--- src/main/java/com/group6/hms/app/screens/patient/ViewAvailableDoctorScreen.java
+++ src/main/java/com/group6/hms/app/screens/patient/ViewAvailableDoctorScreen.java
@@ -1,7 +1,13 @@
 package com.group6.hms.app.screens.patient;
 
+import com.group6.hms.app.auth.LoginManager;
+import com.group6.hms.app.auth.LoginManagerHolder;
 import com.group6.hms.app.managers.AvailabilityManager;
+import com.group6.hms.app.models.Appointment;
 import com.group6.hms.app.models.Availability;
+import com.group6.hms.app.managers.AppointmentManager;
+import com.group6.hms.app.roles.Patient;
+import com.group6.hms.framework.screens.ConsoleColor;
 import com.group6.hms.framework.screens.calendar.CalendarScreen;
 
 import java.time.LocalDate;
@@ -12,18 +18,26 @@ import static java.util.stream.Collectors.groupingBy;
 
 public class ViewAvailableDoctorScreen extends CalendarScreen<Availability, List<Availability>> {
 
-    private final int BOOK_APPOINTMENT = 4;
+    private final int SCHEDULE_APPOINTMENT = 2;
+    private final int RESCHEDULE_APPOINTMENT = 3;
+    private final int CANCEL_APPOINTMENT = 4;
+    private final int VIEW_SCHEDULED_APPOINTMENTS = 5;
+    AvailabilityManager availabilityManager = new AvailabilityManager();
+    AppointmentManager appointmentManager = new AppointmentManager();
+    LoginManager loginManager = LoginManagerHolder.getLoginManager();
+    Patient patient;
 
     /**
      * Constructor to initialize the screen with a title.
      */
     public ViewAvailableDoctorScreen() {
-        super("Available Doctor", null);
-
-        AvailabilityManager availabilityManager = new AvailabilityManager();
+        super("Available Appointments", null);
 
         updateAvailableDoctorsScreen(availabilityManager);
-        addOption(BOOK_APPOINTMENT, "Book Appointment");
+        addOption(SCHEDULE_APPOINTMENT, "Schedule Appointment", ConsoleColor.CYAN);
+        addOption(RESCHEDULE_APPOINTMENT, "Reschedule Appointment", ConsoleColor.CYAN);
+        addOption(CANCEL_APPOINTMENT, "Cancel Appointment", ConsoleColor.CYAN);
+        addOption(VIEW_SCHEDULED_APPOINTMENTS, "View Scheduled Appointments", ConsoleColor.CYAN);
     }
 
     private void updateAvailableDoctorsScreen(AvailabilityManager availabilityManager) {
@@ -35,10 +49,51 @@ public class ViewAvailableDoctorScreen extends CalendarScreen<Availability, List
     protected void handleOption(int optionId) {
         super.handleOption(optionId);
 
-        if(optionId == BOOK_APPOINTMENT) {
-            LocalDate selectedDate = selectedDate();
+        if(optionId == SCHEDULE_APPOINTMENT) {
+            print("Enter the appointment ID that you would like to book: ");
+            String id = readString();
+            Availability availability = availabilityManager.getAvailabilityById(id);
 
+            if(availability == null){
+                setCurrentTextConsoleColor(ConsoleColor.RED);
+                println("No available appointment slots found.");
+            }else{
+                appointmentManager.scheduleAppointment(this.patient, availability);
+                setCurrentTextConsoleColor(ConsoleColor.GREEN);
+                println("Successfully requested for an appointment. To view your appointment status, please go to 'View Schedules Appointments' screen.");
+                updateAvailableDoctorsScreen(availabilityManager);
+            }
+            waitForKeyPress();
+        }else if(optionId == RESCHEDULE_APPOINTMENT) {
+            List<Appointment> patientAppointments = appointmentManager.getAppointmentsByPatient(this.patient);
+
+            print("Enter the appointment ID that you would like to book: ");
+            String id = readString();
+            Availability availability = availabilityManager.getAvailabilityById(id);
+
+            if(availability == null){
+                setCurrentTextConsoleColor(ConsoleColor.RED);
+                println("No available slots to reschedule the appointment.");
+            }else{
+                navigateToScreen(new RescheduleAppointmentScreen(patientAppointments, availability, this.patient));
+            }
+        }else if(optionId == CANCEL_APPOINTMENT) {
+//            List<Appointment> patientAppointments = appointmentManager.getAppointmentsByPatient(this.patient);
+            navigateToScreen(new CancelAppointmentScreen(this.patient));
+        }else if(optionId == VIEW_SCHEDULED_APPOINTMENTS) {
+            navigateToScreen(new ViewScheduledAppointmentsScreen(this.patient));
         }
+    }
 
+    @Override
+    public void onDisplay() {
+        super.onDisplay();
+
+    }
+
+    @Override
+    public void onStart() {
+        this.patient = (Patient) loginManager.getCurrentlyLoggedInUser();
+        super.onStart();
     }
 }
